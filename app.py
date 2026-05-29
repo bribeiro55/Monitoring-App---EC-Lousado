cef6c1d4230b964a0f70c7a14afb3778a694fda9
Ribeiro <uih46657@contiwan.com>
2026-05-29 09:36:31 +0100

    Docker file and pergola.yaml added Debug mode=false & port 0.0.0

--- app.py
+++ app.py
@@ -261,4 +261,4 @@ register_otkph_callbacks(
 scheduler.start()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8050)
+    app.run(debug=False, host="0.0.0.0", port=8050)
